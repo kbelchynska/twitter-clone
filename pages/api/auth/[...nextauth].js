41eb1aaab5e568630f3f6ecc43071837a1d33dd5
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2023-05-04 15:40:26 +0300

    implemented mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,31 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
+import { Session } from "inspector"
 
 export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
   ],
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    session: async ({token: JWT, session: Session}) => {
+      if (session?.user && token?.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    }
+  }
 }
 
 export default NextAuth(authOptions)
--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,27 @@
-import { getProviders } from "next-auth/react"
+import { getProviders, signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/router";
 
 export default function LoginPage({providers}) {
+    const {data, status} = useSession();
+    const router = useRouter();
+    console.log({data, status});
+    if (status === 'loading') {
+        return ''
+    }
+    if (data) {
+        router.push('/');
+    }
     return (
         <div className="flex items-center justify-center h-screen">
             {Object.values(providers).map(provider => (
                 <div>
-                    <button className="bg-twitterWhite px-5 py-2 text-black">Sign in with {provider.name}</button>
+                    <button
+                      onClick={async () => {await signIn(provider.id)}}
+                      className="bg-twitterWhite pl-2 pr-5 py-2 text-black rounded-full flex items-center"
+                    >
+                        <img src="google.png" alt="" className="h-8" />
+                        Sign in with {provider.name}
+                    </button>
                 </div>
             ))}
         </div>
