5fb3e4c20a72f7a196f80c172cdc52dc0d982781
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2023-05-05 15:02:26 +0300

    added users fetch

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+import { useSession } from "next-auth/react"
+import { useEffect } from "react";
+
 export default function Home() {
+  const {data:session, status} = useSession();
+  function getUserInfo() {
+    if (status === 'loading') {
+      return;
+    }
+    fetch('/api/users?id='+session.user.id)
+  }
+  useEffect(() => {
+    getUserInfo();
+  }, [status]);
   return (
     <div>test</div>
   )
