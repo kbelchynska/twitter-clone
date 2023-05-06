133d66d6ffcfff82b928c2d14f32ffdea0f2be41
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2023-05-06 12:05:17 +0300

    pick a username page

---
+++ pages/api/users.js
@@ -0,0 +1,11 @@
+import { initMongoose } from "../../lib/mongoose";
+import User from "../../models/User";
+
+export default async function handle(req, res) {
+    await initMongoose();
+
+    const id = req.query.id;
+    const user = await User.findById(id);
+
+    res.json({user});
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
 import { useSession } from "next-auth/react"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const {data:session, status} = useSession();
+
+  const [userInfo, setUserInfo] = useState();
+  const [userInfoStatus, setUserInfoStatus] = useState('false');
+
   function getUserInfo() {
     if (status === 'loading') {
       return;
     }
     fetch('/api/users?id='+session.user.id)
+      .then(response => {
+        response.json().then(json => {
+          setUserInfo(json.user);
+          setUserInfoStatus('done')
+        })
+      })
   }
+
   useEffect(() => {
     getUserInfo();
   }, [status]);
+
+  if(userInfoStatus === 'loading') {
+    return 'loading user info';
+  }
+
+  if(!userInfo?.username) {
+    return 'no username';
+  }
+
   return (
     <div>test</div>
   )
