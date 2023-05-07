17ea308dfd29f417005680e6bbedb0c12adaf27d
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2023-05-07 15:35:12 +0300

    added username form

---
+++ components/UsernameForm.js
@@ -0,0 +1,9 @@
+export default function UsernameForm {
+    <div className="flex h-screen items-center justify-center">
+        <div className="text-center">
+            <h1 className="text-xl">Pick a username</h1>
+            <input type="text" className="block bm-2" placeholder={'username'} />
+            <button className="block bg-twitterBlue w-full">Continue</button>
+        </div>
+    </div>
+}
