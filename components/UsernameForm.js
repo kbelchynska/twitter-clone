ccd6ac430bc8521fe3ab5087fc293d5219713be4
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2023-05-08 16:07:25 +0300

    updated username form

--- components/UsernameForm.js
+++ components/UsernameForm.js
@@ -1,9 +1,45 @@
-export default function UsernameForm {
-    <div className="flex h-screen items-center justify-center">
-        <div className="text-center">
-            <h1 className="text-xl">Pick a username</h1>
-            <input type="text" className="block bm-2" placeholder={'username'} />
-            <button className="block bg-twitterBlue w-full">Continue</button>
+import useUserInfo from "@/hooks/useUserInfo";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react"
+
+export default function UsernameForm() {
+    const {userInfo, status} = useUserInfo();
+    const [username, setUsername] = useState('');
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === 'loading') {
+            return '';
+        }
+        if (username === '') {
+            const defaultUsername = userinfo?.email?.split('@')[0];
+            setUsername(defaultUsername.repalce(/[^a-z]+/gi, ''));
+        }
+    })
+    async function handleFormSubmit(e) {
+        e.preventDefault();
+        await fetch('/api/users', {
+            method: 'PUT',
+            headers: {'content-type':'application-json'},
+            body: JSON.stringify({username}),
+        });
+        router.reload();
+    }
+
+    
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <form className="text-center" onSubmit={handleFormSubmit}>
+                <h1 className="text-xl">Pick a username</h1>
+                <input
+                  type="text"
+                  className="block mb-1 bg-twitterBorder px-3 py-1 rounded-full"
+                  placeholder={'username'}
+                  value={username}
+                  onChange={e => {setUsername(e.target.value)}}
+                />
+                <button className="block bg-twitterBlue w-full rounded-full py-1">Continue</button>
+            </form>
         </div>
-    </div>
+    )
 }
